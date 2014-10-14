4a982b50566f608a199d02b79ec3050fdb64855a
Kennard Vermeiren <kennardvermeiren@gmail.com>
2014-10-14 13:53:22 +0200

    ReadXML and parse to hashmap

--- SystemY nameServer/src/be/uantwerpen/server/nameServer.java
+++ SystemY nameServer/src/be/uantwerpen/server/nameServer.java
@@ -1,22 +1,75 @@
 package be.uantwerpen.server;
+import java.io.File;
 import java.net.MalformedURLException;
 import java.rmi.AlreadyBoundException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 public class nameServer {
+	
+	static nodeHandling fileImpl;
+	public static nodeHandling getFileImpl() {
+		return fileImpl;
+	}
+	public static void setFileImpl(nodeHandling nodeHandling) {
+		fileImpl = nodeHandling;
+	}
+	
     public static void main(String[] argv) throws RemoteException {
-    	nodeHandling fileImpl = new nodeHandling();
+    	setFileImpl(new nodeHandling());
        String bindLocation = "//localhost/nameServer";
        try { 
 			LocateRegistry.createRegistry(1099);
-			Naming.bind(bindLocation, fileImpl);
+			Naming.bind(bindLocation, getFileImpl());
+			ReadXML();
 	        System.out.println("FileServer Server is ready at:" + bindLocation);
             System.out.println("java RMI registry created.");
         } catch (MalformedURLException | AlreadyBoundException e) {
             System.out.println("java RMI registry already exists.");
         }
        
+    }
+    
+    public static void ReadXML() {
+    	try {
+    		 
+    		File fXmlFile = new File("ip-list.xml");
+    		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+    		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+    		Document doc = dBuilder.parse(fXmlFile);
+
+    		doc.getDocumentElement().normalize();
+    	 
+    	 
+    		NodeList nList = doc.getElementsByTagName("clients");
+    	 
+    	 
+    		for (int temp = 0; temp < nList.getLength(); temp++) {
+    	 
+    			Node nNode = nList.item(temp);
+    	 
+    			//System.out.println("\nCurrent Element :" + nNode.getNodeName());
+    			
+    			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+    	 
+    				Element eElement = (Element) nNode;
+    				
+    				fileImpl.hashMap.put(eElement.getElementsByTagName("hashedName").item(0).getTextContent(), eElement.getElementsByTagName("IP").item(0).getTextContent());
+    			}
+    		}
+    		fileImpl.PrintMap();
+    	    } catch (Exception e) {
+    	    	e.printStackTrace();
+    	    }
+    	
     }
 }
--- SystemY nameServer/src/be/uantwerpen/server/nodeHandling.java
+++ SystemY nameServer/src/be/uantwerpen/server/nodeHandling.java
@@ -9,7 +9,7 @@ import java.net.*;
 
 public class nodeHandling extends UnicastRemoteObject implements nodeHandlingInterface {
 	private static final long serialVersionUID = 1L;
-	private HashMap hashMap = new HashMap();
+	HashMap hashMap = new HashMap();
 	
 	public nodeHandling() throws RemoteException{
 		super();
@@ -18,29 +18,23 @@ public class nodeHandling extends UnicastRemoteObject implements nodeHandlingInt
 	{
 		int hashedName = HashName(name);
 		String clientIP = null;
-		try{
+		try {
 		    System.out.println(clientIP = getClientHost()); // display message
-		    hashMap.put(hashedName, clientIP);
-		    
-		 // Get a set of the entries
-		      Set set = hashMap.entrySet();
-		      // Get an iterator
-		      Iterator i = set.iterator();
-		      // Display elements
-		      while(i.hasNext()) {
-		         Map.Entry me = (Map.Entry)i.next();
-		         System.out.print(me.getKey() + ": ");
-		         System.out.println(me.getValue());
-		      }
-		}catch(Exception e){ clientIP = "none";}
-		
-		
-		
-		
+		    //hashMap.put(hashedName, clientIP);
+		} catch(Exception e) { clientIP = "none";}
 		return hashedName + "";
 	}
-	public void PrintXML() {
-		
+	public void PrintMap() {
+		// Get a set of the entries
+	    Set set = hashMap.entrySet();
+	    // Get an iterator
+	    Iterator i = set.iterator();
+	    // Display elements
+	    while(i.hasNext()) {
+	    	Map.Entry me = (Map.Entry)i.next();
+	        System.out.print(me.getKey() + ": ");
+	        System.out.println(me.getValue());
+	    }
 	}
 	
 	public int HashName(String name) {
