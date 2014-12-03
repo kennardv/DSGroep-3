3450d664a4265d3c30b4b1d05b7ce2b0e1df03f3
NickSegers <segers.nick@live.be>
2014-12-03 11:04:06 +0100

    replicaterUtil

---
+++ Client/src/networking/replicaterUtil.java
@@ -0,0 +1,73 @@
+package networking;
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.rmi.Naming;
+import java.util.List;
+
+import rmi.implementations.NodeToNode;
+import rmi.interfaces.NodeToNodeInterface;
+import utils.Toolkit;
+import enumerations.Mode;
+
+public class replicaterUtil {
+
+    public static void main(String[] args) {
+
+        //define a folder root
+
+    }
+    
+    public void replicate(String[] fileReplicateList, NodeToNode ntn, String rmiSuffixNode, List<File> files, String myIPAddress  )
+    {
+		//get files to replicate
+		if (fileReplicateList == null) {
+			return;
+		}
+		fileReplicateList = ntn.replicationAnswer();
+		for( int i = 0; i< fileReplicateList.length; i++ )
+		{
+			String name = Toolkit.createBindLocation(fileReplicateList[i], rmiSuffixNode);
+			try {
+				TCPUtil tcpSender = new TCPUtil(null, 20000, Mode.SEND, files.get(i), null);
+				Thread t = new Thread(tcpSender);
+				t.start();
+				NodeToNodeInterface ntnI = (NodeToNodeInterface) Naming.lookup(name);
+				ntnI.startReceive(myIPAddress, 20000, files.get(i).getName());
+				t.join();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}	
+			
+		}
+	}
+    
+
+    public void updater()
+    {
+        Path myDir = Paths.get(".\\src\\resources\\replicates");       
+        System.out.println("testing");
+        try {
+           WatchService watcher = myDir.getFileSystem().newWatchService();
+           myDir.register(watcher, StandardWatchEventKinds.ENTRY_CREATE, 
+           StandardWatchEventKinds.ENTRY_DELETE, StandardWatchEventKinds.ENTRY_MODIFY);
+
+           WatchKey watckKey = watcher.take();
+
+           List<WatchEvent<?>> events = watckKey.pollEvents();
+           for (WatchEvent event : events) {
+        	   System.out.println("grfygf");
+                if (event.kind() == StandardWatchEventKinds.ENTRY_CREATE) {
+                    System.out.println("Created: " + event.context().toString());
+                }
+            }
+           
+        } catch (Exception e) {
+            System.out.println("Error: " + e.toString());
+        }
+    }
+}
