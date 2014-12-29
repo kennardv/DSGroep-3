9dc4013d1c584e28c4802c7c668581014016b66f
Kennard Vermeiren <kennardvermeiren@gmail.com>
2014-12-29 15:59:56 +0100

    fixes

--- Client/src/rmi/interfaces/INodeToNode.java
+++ Client/src/rmi/interfaces/INodeToNode.java
@@ -6,7 +6,9 @@ import java.io.IOException;
 import java.net.UnknownHostException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.TreeMap;
 
 public interface INodeToNode extends Remote {
 
@@ -17,5 +19,5 @@ public interface INodeToNode extends Remote {
 	public void startFileRecoveryAgent(FileRecoveryAgent agent) throws RemoteException;
    	public void updatePreviousHash(int hash) throws RemoteException;
    	public void updateNextHash(int hash) throws RemoteException;
-   	public void updateFileList(List<Integer> fileList) throws RemoteException;
+   	public void updateFileList(TreeMap<Integer, Boolean> fileList) throws RemoteException;
 }
